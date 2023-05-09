727e82b602c48e17d12327a9a3d0c9ef235158b0
Aditya Chindhade <adityagc@gmail.com>
2023-05-08 20:42:40 -0700

    exception handling for viz

--- app.py
+++ app.py
@@ -183,73 +183,76 @@ def update_graph(start_date, end_date,input_data, year_value, confidence_interva
     prices of the stock and the second one shows the distribution of forecasted prices and the confidence intervals 
     around them.
     """
-    pio.templates.default = "plotly_dark"
-    trig_id = ctx.triggered_id if not None else 'No clicks yet'
-    #dfticker = df.loc[df['Ticker'] == input_data]
-    dfticker = search_by_ticker(input_data)
-    dftickerpast = dfticker.loc[dfticker['Close'].notnull()]
-    dftickerfuture = dfticker.loc[dfticker['Forecasted Price'].notnull()]
+    try:
+        pio.templates.default = "plotly_dark"
+        trig_id = ctx.triggered_id if not None else 'No clicks yet'
+        #dfticker = df.loc[df['Ticker'] == input_data]
+        dfticker = search_by_ticker(input_data)
+        dftickerpast = dfticker.loc[dfticker['Close'].notnull()]
+        dftickerfuture = dfticker.loc[dfticker['Forecasted Price'].notnull()]
 
-    # Filter dftickerpast to get only the data for the 7 days before the start date of dftickerfuture
-    start_date_f = dftickerfuture['Date'].min() - pd.Timedelta(days=7)
-    end_date_f = dftickerfuture['Date'].min()
-    dftickerpast_filtered = dftickerpast.loc[(dftickerpast['Date'] >= start_date_f) & (dftickerpast['Date'] <= end_date_f)]
+        # Filter dftickerpast to get only the data for the 7 days before the start date of dftickerfuture
+        start_date_f = dftickerfuture['Date'].min() - pd.Timedelta(days=7)
+        end_date_f = dftickerfuture['Date'].min()
+        dftickerpast_filtered = dftickerpast.loc[(dftickerpast['Date'] >= start_date_f) & (dftickerpast['Date'] <= end_date_f)]
 
-    fig1 = go.Figure()
-    fig2 = go.Figure()
+        fig1 = go.Figure()
+        fig2 = go.Figure()
 
-    fig1.add_trace(go.Scatter(x=dftickerpast['Date'], y=dftickerpast['Close'], name='Close'))
-    if trig_id == 'my-date-picker-range':
-        start_date_object = date.fromisoformat(start_date)
-        end_date_object = date.fromisoformat(end_date)
-        fig1.update_xaxes(
+        fig1.add_trace(go.Scatter(x=dftickerpast['Date'], y=dftickerpast['Close'], name='Close'))
+        if trig_id == 'my-date-picker-range':
+            start_date_object = date.fromisoformat(start_date)
+            end_date_object = date.fromisoformat(end_date)
+            fig1.update_xaxes(
+                title='Date',
+                range=(pd.Timestamp(year=start_date_object.year, month=start_date_object.month, day=start_date_object.day, hour=0),
+                    pd.Timestamp(year=end_date_object.year, month=end_date_object.month, day = end_date_object.day, hour=0)),
+                constrain='domain'
+            )
+        else: 
+            fig1.update_xaxes(
+                title='Date',
+                range=(pd.Timestamp(year=year_value[0], month=1, day=1, hour=0),
+                    pd.Timestamp(year=year_value[1], month=date.today().month, day = date.today().day, hour=0)),
+                constrain='domain'
+            )
+
+        
+        fig1.update_layout(title=f'Closing Price of {input_data} over the years')
+
+        fig2.add_trace(go.Scatter(x=dftickerpast_filtered['Date'], y=dftickerpast_filtered['Close'], name='Close'))
+        fig2.add_trace(go.Scatter(x=dftickerfuture['Date'], y=dftickerfuture['Forecasted Price'], name='Forecasted Price'))
+        if confidence_interval == 0.5:
+            lower_col = 'Lower 50'
+            upper_col = 'Upper 50'
+        elif confidence_interval == 0.75:
+            lower_col = 'Lower 75'
+            upper_col = 'Upper 75'
+        elif confidence_interval == 0.95:
+            lower_col = 'Lower 95'
+            upper_col = 'Upper 95'
+        else:
+            lower_col = None
+            upper_col = None
+
+        if lower_col is not None and upper_col is not None:
+            fig2.add_trace(go.Scatter(x=dftickerfuture['Date'], y=dftickerfuture[lower_col], name='', line=dict(color='gray', width=0)))
+            fig2.add_trace(go.Scatter(x=dftickerfuture['Date'], y=dftickerfuture[upper_col], name=f'{int(confidence_interval*100)}% Confidence Interval', fill='tonexty', line=dict(color='gray', width=0)))
+
+        fig2.update_xaxes(
             title='Date',
-            range=(pd.Timestamp(year=start_date_object.year, month=start_date_object.month, day=start_date_object.day, hour=0),
-                   pd.Timestamp(year=end_date_object.year, month=end_date_object.month, day = end_date_object.day, hour=0)),
+            range=(dftickerpast_filtered['Date'].min(), dftickerfuture['Date'].max()),
             constrain='domain'
         )
-    else: 
-        fig1.update_xaxes(
-            title='Date',
-            range=(pd.Timestamp(year=year_value[0], month=1, day=1, hour=0),
-                   pd.Timestamp(year=year_value[1], month=date.today().month, day = date.today().day, hour=0)),
-            constrain='domain'
-        )
 
-    
-    fig1.update_layout(title=f'Closing Price of {input_data} over the years')
-
-    fig2.add_trace(go.Scatter(x=dftickerpast_filtered['Date'], y=dftickerpast_filtered['Close'], name='Close'))
-    fig2.add_trace(go.Scatter(x=dftickerfuture['Date'], y=dftickerfuture['Forecasted Price'], name='Forecasted Price'))
-    if confidence_interval == 0.5:
-        lower_col = 'Lower 50'
-        upper_col = 'Upper 50'
-    elif confidence_interval == 0.75:
-        lower_col = 'Lower 75'
-        upper_col = 'Upper 75'
-    elif confidence_interval == 0.95:
-        lower_col = 'Lower 95'
-        upper_col = 'Upper 95'
-    else:
-        lower_col = None
-        upper_col = None
-
-    if lower_col is not None and upper_col is not None:
-        fig2.add_trace(go.Scatter(x=dftickerfuture['Date'], y=dftickerfuture[lower_col], name='', line=dict(color='gray', width=0)))
-        fig2.add_trace(go.Scatter(x=dftickerfuture['Date'], y=dftickerfuture[upper_col], name=f'{int(confidence_interval*100)}% Confidence Interval', fill='tonexty', line=dict(color='gray', width=0)))
-
-    fig2.update_xaxes(
-        title='Date',
-        range=(dftickerpast_filtered['Date'].min(), dftickerfuture['Date'].max()),
-        constrain='domain'
-    )
-
-    fig1.update_yaxes(title='Price in Dollars')
-    fig2.update_yaxes(title='Price in Dollars')
+        fig1.update_yaxes(title='Price in Dollars')
+        fig2.update_yaxes(title='Price in Dollars')
 
 
-    fig2.update_layout(title=f"Forecasted Price of {input_data} with {int(confidence_interval*100)}% Confidence Interval")
-    return fig1, fig2
+        fig2.update_layout(title=f"Forecasted Price of {input_data} with {int(confidence_interval*100)}% Confidence Interval")
+        return fig1, fig2
+    except:
+        pass
 
 if __name__ == '__main__':
     app.run_server(debug=True)
